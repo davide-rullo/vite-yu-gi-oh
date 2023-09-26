4a99cdd32139d4835ecc700ee7801ca5a5ba3815
davide-rullo <davide.rullo01@gmail.com>
2023-09-26 14:14:53 +0200

    initial commit
    added vite, vue, bootstrap and sass

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script>
+
+
+
+
+export default {
+
+  name: 'App',
+  data() {
+    return {
+
+    }
+  },
+  components: {
+  }
+}
+
+</script>
+
+
+<template>
+  <h1 class="text-center">Ciao</h1>
+</template>
+
+<style lang="scss">
+@use './assets/scss/partials/variables.scss' as *;
+</style>
